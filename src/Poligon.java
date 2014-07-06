5d6f4811620ec4c140d3c7aa629653c9640c69ce
Vidran <vandervidi@gmail.com>
2014-07-06 17:30:37 +0300

    added som classes

--- src/Poligon.java
+++ src/Poligon.java
@@ -1,8 +1,18 @@
+import java.awt.Color;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Poligon {
-	List<Point> points = new ArrayList<>();
+	private List<Point> points;
+	private Point vectorN;
+	private boolean vectorV;   //vectorN * vectorV
+	private Color color;
+	
+public Poligon(List<Point> p, Color c) {
+this.points = p;
+this.color = c;
+
+}
 }
--- src/Triangle.java
+++
@@ -1,8 +0,0 @@
-import java.awt.Point;
-import java.util.ArrayList;
-import java.util.List;
-
-
-public class Triangle {
-	List<Point> points = new ArrayList<>();
-}
